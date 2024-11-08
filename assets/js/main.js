5ec00e9cc7cdbaf53c928fd6cce1abd44c155ea2
Rider7918 <ghostrider7918@gmail.com>
2024-11-08 17:39:02 +0530

    completed push

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,12 +48,12 @@ accordionItems.forEach((item) => {
   const accordionHeader = item.querySelector(".questions__header");
 
   accordionHeader.addEventListener("click", () => {
-    const openItem = document.querySelector('.accordion-open')
+    const openItem = document.querySelector(".accordion-open");
 
     toggleItem(item);
 
-    if(openItem && openItem!== item){
-      toggleItem(openItem)
+    if (openItem && openItem !== item) {
+      toggleItem(openItem);
     }
   });
 });
@@ -71,8 +71,87 @@ const toggleItem = (item) => {
 };
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id");
+      if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+        document
+          .querySelector('.nav__menu a[href*=' + sectionId + ']')
+          .classList.add("active-link");
+      } else {
+        document
+          .querySelector('.nav__menu a[href*=' + sectionId + ']')
+          .classList.remove("active-link");
+      }
+  });
+
+}
+window.addEventListener("scroll", scrollActive);
+
 /*=============== SHOW SCROLL UP ===============*/
 
+function scrollUp(){
+  const scrollUp = document.getElementById('scroll-up')
+  // when the scroll is higher than 200 viewport hight, add show-scroll class to a tag with the scroll-top class
+  if(this.scrollY >= 200) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll',scrollUp)
 /*=============== DARK LIGHT THEME ===============*/
 
+const themeButton =document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
+
+//previously selected topic (if user selected)
+const selectTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+//we obtain  the current theme that the interface has by validating  the dark-theme class
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => document.body.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+
+// we validate if user previously chose a topic
+
+if(selectTheme){
+  //if the validation is fullfilled, we ask what is the issue know to if we activated or deactivated the dark 
+
+  document.body.classList[selectTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactive manually with button
+
+themeButton.addEventListener('click',() =>{
+  //add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme)
+  themeButton.classList.toggle(iconTheme)
+  // we  save the theme  and the current icon that user  chose
+  localStorage.setItem('selected-theme', getCurrentTheme())
+  localStorage.setItem('selected-icon', getCurrentIcon)
+})
+
+
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duraton: 2500,
+  delay: 400,
+  // reset: true
+})
+
+sr.reveal(`.home__data`)
+sr.reveal(`.home__img`,{delay: 500})
+sr.reveal(`.home__social`,{delay: 600})
+sr.reveal(`.about__img, .contact__box`,{origin: 'left'})
+sr.reveal(`.about__data, .contact__form`,{origin: 'right'})
+sr.reveal(`.steps__card, .product__card, .questions__group, .footer`,{interval: 100})
