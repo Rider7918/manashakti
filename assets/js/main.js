f218563f60bf40a51e3b89ec3437b65f59c555a0
Rider7918 <ghostrider7918@gmail.com>
2024-11-30 07:49:27 +0530

    added insta link to footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -150,8 +150,8 @@ const sr = ScrollReveal({
 })
 
 sr.reveal(`.home__data`)
-sr.reveal(`.home__img`,{delay: 500})
-sr.reveal(`.home__social`,{delay: 600})
+sr.reveal(`.home__img`,{delay: 200})
+sr.reveal(`.home__social`,{delay: 400})
 sr.reveal(`.about__img, .contact__box, .experience__company`,{origin: 'left'})
 sr.reveal(`.about__data, .contact__form, .experience__info`,{origin: 'right'})
 sr.reveal(`.steps__card, .product__card, .questions__group, .footer`,{interval: 100})
